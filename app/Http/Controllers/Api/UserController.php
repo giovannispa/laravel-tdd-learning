0211906b0cd7a3df7bc33be8289f5e3b651a6890
Giovanni Sertorio <giovanni.spadoni10@hotmail.com>
2023-08-31 14:14:15 -0300

    Feature // Adicionei os comentários que faltavam nos actions do controller de usuário.

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -17,6 +17,11 @@ class UserController extends Controller
         $this->repository = $repository;
     }
 
+    /**
+     * Action principal da api de listagem de usuários.
+     *
+     * @return \Illuminate\Http\Resources\Json\AnonymousResourceCollection
+     */
     public function index()
     {
         $users = collect($this->repository->findAll());
@@ -24,6 +29,11 @@ class UserController extends Controller
         return UserResource::collection($users);
     }
 
+    /**
+     * Action que gerencia toda listagem de usuários por página.
+     *
+     * @return \Illuminate\Http\Resources\Json\AnonymousResourceCollection
+     */
     public function paginate()
     {
         $response = $this->repository->paginate();
@@ -38,6 +48,12 @@ class UserController extends Controller
         ]);
     }
 
+    /**
+     * Action que salva o usuário no banco.
+     *
+     * @param UserRequest $request
+     * @return UserResource
+     */
     public function store(UserRequest $request)
     {
         $user = $this->repository->create($request->all());
@@ -45,6 +61,12 @@ class UserController extends Controller
         return new UserResource($user);
     }
 
+    /**
+     * Action que retorna o usuário especifico no endpoint.
+     *
+     * @param $email
+     * @return UserResource
+     */
     public function show($email)
     {
         $user = $this->repository->find($email);
@@ -52,11 +74,24 @@ class UserController extends Controller
         return new UserResource(collect($user));
     }
 
+    /**
+     * Action que atualiza um usuário existente no banco.
+     *
+     * @param UserRequest $request
+     * @param $email
+     * @return UserResource
+     */
     public function update(UserRequest $request, $email)
     {
         return new UserResource(collect($this->repository->update($email, $request->all())));
     }
 
+    /**
+     * Action que deleta um usuário especifico no banco.
+     *
+     * @param $email
+     * @return \Illuminate\Http\Response
+     */
     public function destroy($email)
     {
         $this->repository->delete($email);
